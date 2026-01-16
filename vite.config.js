640d9a40006a354c0c3d34c9003da21c537e2376
garry.l.tcg <garry.l.tcg@gmail.com>
2026-01-16 15:46:26 +0800

    fix: set base for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src')
-    }
-  }
-})
+      "@": resolve(__dirname, "src"),
+    },
+  },
+  base: "/radar-fraud-ui/",
+});
